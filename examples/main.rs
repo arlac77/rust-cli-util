3737b5dfed306e7a2307adb85beab1157eafe305
Markus Felten <Markus.Felten@gmx.de>
2021-01-06 23:55:56 +0100

    fix:impl Popup

--- examples/main.rs
+++ examples/main.rs
@@ -1,12 +1,12 @@
 extern crate termion;
 
 use std::io::{stdin, stdout, Write};
+use termion::color;
 use termion::event::{Event, Key};
 use termion::input::{MouseTerminal, TermRead};
 use termion::raw::IntoRawMode;
-use termion::{color};
 
-use cli_util::popup;
+use cli_util::Popup;
 
 fn main() {
     let stdin = stdin();
@@ -24,12 +24,31 @@ fn main() {
     .unwrap();
     stdout.flush().unwrap();
 
+    let mut pp = Popup {
+        x: 1,
+        y: 1,
+        title: "title",
+        entries: vec!["a", "b", "c"],
+        selected: 0,
+        visible: false,
+    };
+
     let mut stdin_iter = stdin.events();
     while let Some(c) = stdin_iter.next() {
         let evt = c.unwrap();
+
+        pp.draw(&mut stdout);
+
         match evt {
             Event::Key(Key::Char('q')) => break,
-            Event::Key(Key::Char('p')) => popup(&mut stdout, x as usize, y as usize, "title", vec!["a", "b", "c"], y as usize),
+            Event::Key(Key::Esc) => {
+                pp.visible = false;
+                pp.draw(&mut stdout);},
+
+            Event::Key(Key::Char('p')) => {
+                pp.visible = true;
+                pp.draw(&mut stdout);
+            }
             Event::Key(Key::Char(c)) => {
                 write!(stdout, "{}{}", termion::cursor::Goto(x, y), c).unwrap();
                 x = x + 1
@@ -38,11 +57,20 @@ fn main() {
             Event::Key(Key::Ctrl(c)) => println!("Ctrl-{}", c),
             Event::Key(Key::Left) => x = x - 1,
             Event::Key(Key::Right) => x = x + 1,
-            Event::Key(Key::Up) => y = y - 1,
-            Event::Key(Key::Down) => y = y + 1,
+            Event::Key(Key::Up) => {
+                y = y - 1;
+                if (pp.visible) {
+                    pp.selected = pp.selected - 1
+                }
+            }
+            Event::Key(Key::Down) => {
+                y = y + 1;
+                if (pp.visible) {
+                    pp.selected = pp.selected + 1
+                }
+            }
             _ => {}
         }
         stdout.flush().unwrap();
     }
 }
-
--- src/lib.rs
+++ src/lib.rs
@@ -6,39 +6,52 @@ use termion::input::{MouseTerminal, TermRead};
 use termion::raw::IntoRawMode;
 use termion::{color};
 
-pub fn popup(out: &mut dyn Write, x: usize, y: usize, title: &str, entries: Vec<&str>, selected: usize) {
-    let w = 16;
-    let bar = "═".repeat(w);
+pub struct Popup {
+    pub x: usize,
+    pub y: usize,
+    pub title: &'static str,
+    pub entries: Vec<&'static str>,
+    pub selected: usize,
+    pub visible: bool
+}
 
-    write!(
-        out,
-        "{}╔{}╗\n\r║{}{}║\n\r",
-        termion::cursor::Goto(x as u16, y as u16),
-        bar,
-        title,
-        termion::cursor::Goto((x + w) as u16 + 1, y as u16 + 1)
-    )
-    .unwrap();
-
-    let mut i = 0;
-
-    for e in &entries {
-        let (color, uncolor) = if i == selected {
-            (color::Green.fg_str(), color::Black.fg_str())
-        } else {
-            ("", "")
-        };
-
+impl Popup {
+    pub fn draw(&self, out: &mut dyn Write) {
+        if(!self.visible) { return }
+        
+        let w = 16;
+        let bar = "═".repeat(w);
+    
         write!(
             out,
-            "║ {}{}{}{}║\n\r",
-            color,
-            e,
-            uncolor,
-            termion::cursor::Goto((x + w) as u16 + 1, (y + i) as u16 + 2)
+            "{}╔{}╗\n\r║{}{}║\n\r",
+            termion::cursor::Goto(self.x as u16, self.y as u16),
+            bar,
+            self.title,
+            termion::cursor::Goto((self.x + w) as u16 + 1, self.y as u16 + 1)
         )
         .unwrap();
-        i = i + 1;
+    
+        let mut i = 0;
+    
+        for e in &self.entries {
+            let (color, uncolor) = if i == self.selected {
+                (color::Green.fg_str(), color::Black.fg_str())
+            } else {
+                ("", "")
+            };
+    
+            write!(
+                out,
+                "║ {}{}{}{}║\n\r",
+                color,
+                e,
+                uncolor,
+                termion::cursor::Goto((self.x + w) as u16 + 1, (self.y + i) as u16 + 2)
+            )
+            .unwrap();
+            i = i + 1;
+        }
+        write!(out, "╚{}╝", bar).unwrap();
     }
-    write!(out, "╚{}╝", bar).unwrap();
 }
